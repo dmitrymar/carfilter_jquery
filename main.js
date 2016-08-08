e9db805296cb53e5993bb45842513e2112450f27
Dmitry Mardukhayev <dmitrymar@yahoo.com>
2016-08-08 15:18:40 -0400

    added director script

--- main.js
+++ main.js
@@ -112,6 +112,24 @@
             console.log("route")
             if (window.location.search) {
                 this.requestData(window.location.search.substr(1));
+                var author = function () { console.log("author"); };
+                var books = function () { console.log("books"); };
+                var viewBook = function (bookId) {
+                  console.log("viewBook: bookId is populated: " + bookId);
+                };
+
+                var routes = {
+                  '/author': author,
+                  '/books': [books, function() {
+                    console.log("An inline route handler.");
+                  }],
+                  '/books/view/:bookId': viewBook
+                };
+
+                var router = Router(routes);
+
+                router.init();
+
             } else {
                 this.requestData();
             }
